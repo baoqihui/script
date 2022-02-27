d5d0751bed7454684c2408f2883ec49a3b53d8ba
baoqihui <50536515+baoqihui@users.noreply.github.com>
2022-02-27 14:21:45 +0800

    Create root.sh

---
+++ root.sh
@@ -0,0 +1,16 @@
+echo " --------------------------------------------------------------------"
+echo -e " ------------------------ \033[33m一键修改root账户密码\033[0m -------------------------- "
+echo " --------------------------------------------------------------------"
+
+echo -e "\033[32m 开始执行... \033[0m"
+
+read -p " 请输入你root密码:" password                                   
+password=${password}
+if test ! -z "${password}"
+	then 
+		echo -e "\033[32m password:$password \033[0m"
+	else
+		echo -e "\033[33m password为空，退出程序... \033[0m"
+		exit
+fi
+echo root:$password|chpasswd
