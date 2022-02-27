a4517d6ceee51526136c24e7459a334d8b11d2b0
baoqihui <50536515+baoqihui@users.noreply.github.com>
2022-02-27 15:22:21 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,15 +1,17 @@
 echo " --------------------------------------------------------------------"
 echo -e " ------------------------ \033[33m一键修改root账户密码\033[0m -------------------------- "
 echo " --------------------------------------------------------------------"
-echo -e "\033[32m 开始执行... \033[0m"
 
-sudo -i
-sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
-sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
-service sshd restart
-exit
 
 echo -e "\033[32m 开始修改密码... \033[0m"
 sudo passwd root
+
+echo -e "\033[32m 开始执行... \033[0m"
+
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sudo service sshd restart
+
+
 
 
