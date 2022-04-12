4ed6771f17561ebdc01cf2936b0d0e9bcb975559
baoqihui <50536515+baoqihui@users.noreply.github.com>
2022-04-12 15:39:47 +0800

    Update reciver-mysql.sh

--- reciver-mysql.sh
+++ reciver-mysql.sh
@@ -5,5 +5,5 @@ datas=${datas:-'admin alist backend cms cms-plus demo student teacher wms'}
 for i in $datas;  
 do  
 	echo "$nowTime: 还原$i数据库"
-	docker exec mysql sh -c 'mysql -uroot -pHBQ521521cf* < /out/mysql/20220412/$i.sql';
+	docker exec mysql sh -c "mysql -uroot -pHBQ521521cf* < /out/mysql/20220412/$i.sql";
 done  
