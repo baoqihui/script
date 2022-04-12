2c1fc866f02be2ecc82b0f85074719b9ea1d34c8
baoqihui <50536515+baoqihui@users.noreply.github.com>
2022-04-12 15:47:04 +0800

    Update reciver-mysql.sh

--- reciver-mysql.sh
+++ reciver-mysql.sh
@@ -1,9 +1,11 @@
 #需要还原的文件列表
-read -p " 请输入你要还原的数据库(多个空格隔开):" datas
-datas=${datas:-'admin alist backend cms cms-plus demo student teacher wms'}
+nowDate=$(date "+%Y%m%d")
+read -p " 请输入你要还原的数据库容器中绝对路径(多个空格隔开):" datas
+datas=${datas:-"/out/mysql/$nowDate/all.sql"}
 #遍历数据库进行还原
 for i in $datas;  
 do  
+	nowTime=$(date "+%Y-%m %d-%H:%M:%S")
 	echo "$nowTime: 还原$i数据库"
-	docker exec mysql sh -c "mysql -uroot -pHBQ521521cf* < /out/mysql/20220412/$i.sql";
+	docker exec mysql sh -c "mysql -uroot -pHBQ521521cf* < $i";
 done  
