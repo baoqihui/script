181a01fb66c84485a3302829b58e040c39e191e9
baoqihui <50536515+baoqihui@users.noreply.github.com>
2022-04-12 16:35:56 +0800

    Update my-backup-mysql.sh

--- my-backup-mysql.sh
+++ my-backup-mysql.sh
@@ -1,28 +1,28 @@
-# 需要备份的数据库目录，多个空格隔开
-datas="admin alist backend cms cms-plus demo student teacher wms"
+#备份数据库名字
+name="all-db"
 # 备份文件存放地址(根据实际情况填写)
 outDir="/out/mysql"
 # 是否删除过期数据
-need_delete="YES"
+need_delete="ON"
 #过期时长，单位 天
 expire_days=2
-#当前日期，用于生成目录
-nowDate=$(date "+%Y%m%d")
 #记录当前时间
-nowTime=$(date "+%Y-%m %d-%H:%M:%S")
+nowTime=$(date "+%Y%m%d")
+#最终文件名
+finalName=$name-$nowTime.sql
+#最终文件路径
+finalPath=$outDir/$finalName
 
 # 创建目录
-mkdir -p $outDir/$nowDate
+mkdir -p $outDir
 
-#遍历数据库进行备份
-for i in $datas;  
-do  
-	echo "$nowTime: 备份$i数据库到'$outDir/$nowDate/$i.sql'"
-	docker exec -i mysql mysqldump -B $i > $outDir/$nowDate/$i.sql
-done  
+#进行备份
+echo "$nowTime: 备份所有数据库到：$finalPath"
+docker exec -i mysql mysqldump -A > $finalPath
+ 
 
 # 删除过期数据
 if [ "$need_delete" == "ON" -a  "$outDir" != "" ];then
-   find $outDir/* -type d -mtime +1 | xargs rm -rf
+   find $outDir -type f -mtime +$expire_days | xargs rm -rf
    echo "$nowTime: Expired backup data delete complete!"
 fi
