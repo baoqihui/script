3179180fbc5d84e5de3d4621ba7b9e00964a94dc
hui <huibq@cheche365.com>
2022-04-20 13:46:52 +0800

    恢复mysql8

--- recover-ali.sh
+++ recover-ali.sh
@@ -1,6 +1,6 @@
 #需要还原的文件列表
 read -p " 请输入你要还原的文件绝对路径(多个空格隔开):" datas
-datas=${datas:-'/backup/opt.zip /backup/mysql.zip'}
+datas=${datas:-'/backup/opt.zip /backup/mysql.zip /backup/mysql8.zip'}
 #还原位置
 outDir="/out/recover"
 mkdir -p $outDir
