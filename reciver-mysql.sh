166c76e755748b245531f873a6f436bc8f4d476e
hui <huibq@cheche365.com>
2022-04-20 11:56:51 +0800

    恢复mysql，容器名自定义

--- reciver-mysql.sh
+++ reciver-mysql.sh
@@ -1,13 +1,15 @@
+read -r " 请输入你要还原的数据库容器名:" name
+name=${name:-"mysql"}
 #需要还原的文件列表
 nowDate=$(date "+%Y%m%d")
-read -p " 请输入你要还原的数据库容器中绝对路径(多个空格隔开):" datas
+read -r " 请输入你要还原的数据库容器中绝对路径(多个空格隔开):" datas
 datas=${datas:-"/out/mysql/all-db-$nowDate.sql"}
-read -p " 数据库密码(脚本不记录):" password
+read -r " 请输入数据库密码(脚本不记录):" password
 password=${password:-"HBQ521521cf*"}
 #遍历数据库进行还原
-for i in $datas;  
+for i in $datas;
 do  
 	nowTime=$(date "+%Y-%m %d-%H:%M:%S")
-	echo "$nowTime: 还原$i数据库"
-	docker exec mysql sh -c "mysql -uroot -p$password < $i";
+	echo "$nowTime: 还原 $i 数据库到 $name 容器中"
+	docker exec "$name" sh -c "mysql -uroot -p$password < $i";
 done  
